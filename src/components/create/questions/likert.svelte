2500be1b27be94eb48ab822f01f90a9a340c35d8
Carter <johnston.j.carter@gmail.com>
2022-10-22 16:58:26 -0400

    sbcadjs-7 added likert question component

---
+++ src/components/create/questions/likert.svelte
@@ -0,0 +1,34 @@
+<script>
+	let columns = [];
+	let rows = [];
+	
+	let newRow = "row";
+	let newColumn = "Col";
+</script>
+
+<table>
+	<tr>
+		<th>Question</th>
+		{#each columns as column}
+		<th contenteditable="true">{column}</th>
+		{/each}
+	</tr>
+	{#each rows as item}
+		<tr>
+			<td contenteditable="true">row</td>
+			{#each columns as column}
+			<td><input type="radio"></td>
+			{/each}
+		</tr>
+	{/each}
+</table>
+
+<button on:click={() => {
+	rows.push(newRow);
+	rows = rows;
+}}>Add Row</button>
+
+<button on:click={() => {
+	columns.push(newColumn);
+	columns = columns;
+}}>Add Column</button>
