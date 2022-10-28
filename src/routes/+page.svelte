358a5d98cb618a2ea6d03c869e07ed41a4149d2a
Carter <johnston.j.carter@gmail.com>
2022-10-27 20:56:10 -0400

    wip

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<h1>Welcome to the Yoga 4 Change Survey System!</h1>
+
+<style>
+    h1 {
+      justify-content: center;  
+    }
+</style>
--- src/routes/create/+page.svelte
+++ src/routes/create/+page.svelte
@@ -1,6 +1,4 @@
 <script>
-    import { each } from "svelte/internal";
-
     const currentDate = new Date();
     let componentList = [];
     let newRow = "likert";
@@ -13,24 +11,10 @@
 
     <label for="createDate">Creation date:</label>
     <input type="text" placeholder={currentDate} readonly /><br />
-    <hr />
-    <div class="list-group">
-        {#each componentList as component}
-            <div>
-              <h5>Heading</h5>
-              <small>ADD: useful tip on how to edit question.</small>
-            </div>
-        {/each}
-  
-        
-    </div>
-    <button class="btn" on:click={() => {
-        componentList.push(newRow);
-        componentList = componentList;
-       
-    }}>
-        <img src="" alt="+" /> Add Question
-    </button>
+
+    
+    <button class="btn" on:click={() => {}}>Add Question</button>
+
     <select class="" id="questionSelector">
         <option value="likert">Likert</option>
         <option value="slider">Slider</option>
@@ -38,38 +22,3 @@
         <option value="dropdown">Dropdown</option>
     </select>
 </form>
-
-<!-- <script>
-	let columns = [];
-	let rows = [];
-	
-	let newRow = "row";
-	let newColumn = "Col";
-</script>
-
-<table>
-	<tr>
-		<th>Question</th>
-		{#each columns as column}
-		<th contenteditable="true">{column}</th>
-		{/each}
-	</tr>
-	{#each rows as item}
-		<tr>
-			<td contenteditable="true">row</td>
-			{#each columns as column}
-			<td><input type="radio"></td>
-			{/each}
-		</tr>
-	{/each}
-</table>
-
-<button on:click={() => {
-	rows.push(newRow);
-	rows = rows;
-}}>Add Row</button>
-
-<button on:click={() => {
-	columns.push(newColumn);
-	columns = columns;
-}}>Add Column</button> -->
