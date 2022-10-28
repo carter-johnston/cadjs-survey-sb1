a4494f3385f32a788b59aa97fa272266e79b9131
Dhori Mato <dhori.mato@gmail.com>
2022-10-27 21:13:12 -0400

    More survey

--- src/routes/survey/+page.svelte
+++ src/routes/survey/+page.svelte
@@ -1,21 +1,41 @@
 <h1>Survey #1</h1>
 <br />
 <form action="POST">
-  <div class="userName">
-    <label for="userId">
-      Please enter a username. The username should be the first two letters of
+  <div class="mb-3">
+    <label for="exampleFormControlInput1" class="form-label"
+      >Please enter a username. The username should be the first two letters of
       your first name, the first two letters of your last name followed by the
       month and day of your birthday. For example, John Doe, born September 5th
-      would have the following username: JODO0905
-    </label>
-    <input type="text" class="userId" placeholder="JODO0905" />
+      would have the following username: JODO0905</label
+    >
+    <input
+      type="email"
+      class="form-control"
+      id="exampleFormControlInput1"
+      placeholder="JODO0905"
+    />
   </div>
   <ul class="likert">
-    <li>Strongly Disagree <input type="radio" class="likert" /></li>
-    <li>Somewhat Disagree <input type="radio" class="likert" /></li>
-    <li>Neutral <input type="radio" class="likert" /></li>
-    <li>Somehwat Agree <input type="radio" class="likert" /></li>
-    <li>Strongly Agree <input type="radio" class="likert" /></li>
+    <li>
+      <input type="radio" name="likert" value="strong_agree" />
+      <label for="radio">Strongly agree</label>
+    </li>
+    <li>
+      <input type="radio" name="likert" value="strong_agree" />
+      <label for="radio">Agree</label>
+    </li>
+    <li>
+      <input type="radio" name="likert" value="strong_agree" />
+      <label for="radio">Neutral</label>
+    </li>
+    <li>
+      <input type="radio" name="likert" value="disagree" />
+      <label for="radio">Disagree</label>
+    </li>
+    <li>
+      <input type="radio" name="likert" value="strong_agree" />
+      <label for="radio">Strongly disagree</label>
+    </li>
   </ul>
 </form>
 
@@ -24,8 +44,45 @@
     text-align: center;
   }
 
-  .userName {
-    display: grid;
-    margin: 0px 35px;
+  .mb-3 {
+    margin: 0px 25px;
+  }
+
+  form .likert {
+    list-style: none;
+    width: 100%;
+    margin: 0;
+    padding: 0 0 35px;
+    display: block;
+    border-bottom: 2px solid #efefef;
+  }
+  form .likert:last-of-type {
+    border-bottom: 0;
+  }
+  form .likert:before {
+    content: '';
+    position: relative;
+    top: 11px;
+    left: 9.5%;
+    display: block;
+    background-color: #efefef;
+    height: 4px;
+    width: 78%;
+  }
+  form .likert li {
+    display: inline-block;
+    width: 19%;
+    text-align: center;
+    vertical-align: top;
+  }
+  form .likert li input[type='radio'] {
+    display: block;
+    position: relative;
+    top: 0;
+    left: 50%;
+    margin-left: -6px;
+  }
+  form .likert li label {
+    width: 100%;
   }
 </style>
