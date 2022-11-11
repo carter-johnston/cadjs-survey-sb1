dd0de0b7726d7b891f0c9d1cfb3763c81e002e0a
Dhori Mato <dhori.mato@gmail.com>
2022-11-11 10:51:18 -0500

    Static success and survey pages

--- src/routes/create/+page.svelte
+++ src/routes/create/+page.svelte
@@ -1,42 +1,42 @@
 <script>
-    import { each } from "svelte/internal";
+  import { each } from 'svelte/internal';
 
-    const currentDate = new Date();
-    let componentList = [];
-    let newRow = "likert";
+  const currentDate = new Date();
+  let componentList = [];
+  let newRow = 'likert';
 </script>
 
 <h1>Create New Survey</h1>
 <form action="">
-    <label for="surveyName">Survey name:</label>
-    <input type="text" /><br />
+  <label for="surveyName">Survey name:</label>
+  <input type="text" /><br />
 
-    <label for="createDate">Creation date:</label>
-    <input type="text" placeholder={currentDate} readonly /><br />
-    <hr />
-    <div class="list-group">
-        {#each componentList as component}
-            <div>
-              <h5>Heading</h5>
-              <small>ADD: useful tip on how to edit question.</small>
-            </div>
-        {/each}
-  
-        
-    </div>
-    <button class="btn" on:click={() => {
-        componentList.push(newRow);
-        componentList = componentList;
-       
-    }}>
-        <img src="" alt="+" /> Add Question
-    </button>
-    <select class="" id="questionSelector">
-        <option value="likert">Likert</option>
-        <option value="slider">Slider</option>
-        <option value="text">Textbox</option>
-        <option value="dropdown">Dropdown</option>
-    </select>
+  <label for="createDate">Creation date:</label>
+  <input type="text" placeholder={currentDate} readonly /><br />
+  <hr />
+  <div class="list-group">
+    {#each componentList as component}
+      <div>
+        <h5>Heading</h5>
+        <small>ADD: useful tip on how to edit question.</small>
+      </div>
+    {/each}
+  </div>
+  <button
+    class="btn"
+    on:click={() => {
+      componentList.push(newRow);
+      componentList = componentList;
+    }}
+  >
+    <img src="" alt="+" /> Add Question
+  </button>
+  <select class="" id="questionSelector">
+    <option value="likert">Likert</option>
+    <option value="slider">Slider</option>
+    <option value="text">Textbox</option>
+    <option value="dropdown">Dropdown</option>
+  </select>
 </form>
 
 <!-- <script>
