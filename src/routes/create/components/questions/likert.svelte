89a9dea471e5c424fc84fb8d3cc5aaa210240d56
Carter <johnston.j.carter@gmail.com>
2022-12-01 13:16:27 -0500

    clean up with likert component

--- src/routes/create/components/questions/likert.svelte
+++ src/routes/create/components/questions/likert.svelte
@@ -1,8 +1,8 @@
 <script>
 	export let questionNumber;
-
 	let columns = [];
 	let rows = [];
+
 	const defaultRow = "new question.";
 	const defaultColumn = "new label.";
 </script>
@@ -16,21 +16,20 @@
 		<th  contenteditable="true" bind:textContent={column}>{column}</th>
 		{/each}
 	</tr>
-	{#each rows as item}
+	{#each rows as row}
 		<tr>
-			<td contenteditable="true" bind:textContent={item}>{item}</td>
+			<td contenteditable="true" bind:textContent={row}>{row}</td>
 			{#each columns as col}
 				<td><input type="radio"></td>
 			{/each}
 		</tr>
 	{/each}
 </table>
+
 <button class="btn btn-secondary" on:click={() => {
-	let newRow = rows.push(defaultRow);
-	rows = newRow;
+	rows = [...rows, defaultRow ];
 }}>Add Row</button>
 
 <button class="btn btn-secondary" on:click={() => {
-	let newColumn = columns.push(defaultColumn);
-	columns = newColumn;
+	columns = [...columns, defaultColumn ];
 }}>Add Column</button>
