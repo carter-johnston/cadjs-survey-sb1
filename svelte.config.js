a0324faed07a03b253c552a1cbadfed99d3939b8
Carter <johnston.j.carter@gmail.com>
2022-11-30 22:37:39 -0500

    hooked up azure adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,10 @@
 import adapter from '@sveltejs/adapter-node';
+import azure from 'svelte-adapter-azure-swa';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: azure()
 	}
 };
 
