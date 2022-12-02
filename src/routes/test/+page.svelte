9935bd3094b56d026045fd14596d542c464b95d5
Windz-GameDev <aaronjgoldstein07@gmail.com>
2022-12-02 15:51:48 -0500

    Added database connection to local deployment

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    let todo = {};
+
+    async function submit() {
+        const response = await fetch("", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify(todo),
+        });
+    }
+</script>
+
+<form method="POST" on:submit|preventDefault={submit}>
+    <input type="text" bind:value={todo.name} />
+    <button type="submit">Submit</button>
+</form>
