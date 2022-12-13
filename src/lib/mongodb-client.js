9da61a23831323f45a90ce08b46fd0de07733727
Jacob Schipman <Jacobschipman@hotmail.com>
2022-12-13 03:11:00 -0500

    Temp save

--- src/lib/mongodb-client.js
+++ src/lib/mongodb-client.js
@@ -1,12 +1,13 @@
 import { MongoClient, ServerApiVersion } from 'mongodb';
-// import dotenv from 'dotenv';
-// dotenv.config();
+import dotenv from `dotenv`
+ 
+dotenv.config();
 
 
 
 //TODO: add to env
 const credentials = 'X509-cert-5650174497133697990.pem'
-const client = new MongoClient('mongodb+srv://cadjs-cluster.ekarosn.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority', {
+const client = new MongoClient(process.env.MONGODB_URI, {
     sslKey: credentials,
     sslCert: credentials,
     serverApi: ServerApiVersion.v1
