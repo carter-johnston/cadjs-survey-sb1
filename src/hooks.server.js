1c60d927b3c1c07fa4cd2bb5df2a3b2bd4730a57
Carter <johnston.j.carter@gmail.com>
2022-12-26 14:12:55 -0500

    active survey deletion functionality

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,8 +1,10 @@
-import { start_mongodb } from '$lib/mongodb_client';
+import { start_mongodb } from "$lib/mongodb_client";
 
-start_mongodb().then(() => {
-    console.log('Connection to MongoDb started.');
-}).catch((error) => {
+start_mongodb()
+  .then(() => {
+    console.log("Connection to MongoDb started.");
+  })
+  .catch((error) => {
     error.log();
-    console.error('Not able to establish a connection to MongoDb');
-});
+    console.error("Not able to establish a connection to MongoDb");
+  });
