e1950a8984affcedac7405cf86736c07f4eb7157
Windz-GameDev <aaronjgoldstein07@gmail.com>
2022-12-28 21:34:34 -0500

    Adding consistent styling for analysis page

--- src/routes/active/+page.svelte
+++ src/routes/active/+page.svelte
@@ -1,7 +1,7 @@
 <script>
-	import { copy } from 'svelte-copy';
-	import { jsPDF } from 'jspdf';
-	import { page } from '$app/stores';
+	import { copy } from "svelte-copy";
+	import { jsPDF } from "jspdf";
+	import { page } from "$app/stores";
 	const path = $page.url;
 
 	//TODO consult with Dhori on building out the survey.
@@ -17,10 +17,10 @@
 	async function deleteSurvey(surveyName) {
 		if (!surveyName) {
 			const urlArgument = new URLSearchParams();
-			urlArgument.set('surveyName', `${surveyName}`);
+			urlArgument.set("surveyName", `${surveyName}`);
 			const url = `${path}?${urlArgument}`;
 			const res = await fetch(url, {
-				method: 'DELETE',
+				method: "DELETE",
 			});
 			const data = await res.json();
 			console.log(data);
@@ -33,13 +33,14 @@
 
 <div class="ms-5 me-5 mt-2">
 	<h1 class="mb-3">Active Surveys</h1>
-	<div class="lead ms-3">
-		Here is a list of existing surveys. Select <i>Activate</i> on the survey
-		you with to share. Once activated, will only be available for a short time.
+	<div class="lead ms-3 mb-3">
+		Here is a list of existing surveys. Select the <i>"Activate"</i> button on
+		the survey that you wish to share. Once activated, that survey will only
+		be available until the set expiration date or until you close it.
 	</div>
 	<div class="list-group">
 		{#each data.activeSurveys as survey}
-			<div class="container border border-rounded m-1 p-2">
+			<div class="container border border-rounded m-2 p-2">
 				<div class="d-flex p-2">
 					<strong>
 						{survey.surveyName}
@@ -54,30 +55,35 @@
 
 					<div class="flex-fill">
 						<strong>Created:</strong>
-						{survey.creationDate}
+						{survey.dateCreated}
 					</div>
 				</div>
-				<div class="d-flex ms-2">
+				<div class="d-flex p-2">
 					<strong>Number of Questions:</strong>
-					{survey.numQuestions}
+					{survey.numOfQuestions}
 				</div>
 
 				<div class="d-flex p-2">
-					<button class="btn btn-success" on:click="{() => {}}"
-						>Activate</button>
 					<button
-						class="btn btn-outline-secondary"
-						use:copy="{window.location.host + '/survey'}"
-						>Copy Link to Clipboard</button>
+						class="btn btn-outline-success me-2"
+						on:click={() => {}}>Activate</button
+					>
 					<button
-						class="btn btn-outline-secondary"
-						on:click="{createPDF(survey)}"
-						>Download Link as PDF</button>
-					<button class="btn btn-outline-secondary">Edit</button>
+						class="btn btn-outline-secondary me-2"
+						use:copy={window.location.host + "/survey"}
+						>Copy Link to Clipboard</button
+					>
 					<button
-						class="btn btn-outline-danger"
-						on:click="{deleteSurvey(survey.surveyName)}"
-						>Delete</button>
+						class="btn btn-outline-secondary me-2"
+						on:click={createPDF(survey)}
+						>Download Link as PDF</button
+					>
+					<button class="btn btn-outline-secondary me-2">Edit</button>
+					<button
+						class="btn btn-outline-danger me-2"
+						on:click={deleteSurvey(survey.surveyName)}
+						>Delete</button
+					>
 				</div>
 			</div>
 		{:else}
@@ -85,9 +91,3 @@
 		{/each}
 	</div>
 </div>
-
-<style>
-	/* span {
-		display: flex;
-	} */
-</style>
--- src/routes/analysis/+page.svelte
+++ src/routes/analysis/+page.svelte
@@ -1,76 +1,59 @@
-<h1>Analysis</h1>
-<head>
-
-</head>
 <script>
-
-
-    import { add_flush_callback, exclude_internal_props, prevent_default } from "svelte/internal";
-    import { saveAs } from 'file-saver';
-    import * as XLSX from 'xlsx/xlsx.mjs';
-    
-    
-    
-
-
+    import {
+        add_flush_callback,
+        exclude_internal_props,
+        prevent_default,
+    } from "svelte/internal";
+    import { saveAs } from "file-saver";
+    import * as XLSX from "xlsx/xlsx.mjs";
 
     let surveyList = [
         {
-                    id:0,
-                    surveyName: "Quest4Change",
-                    surveyAuthor: "Aaron J. Goldstein",
-                    numQuestions: "4",
-                    creationDate: "10/27/2022",
-                    Questions: ['ID', 'Age', 'Likert1', 'Likert2' ],
-                    Entry: ['AJG', '22', '3', '5'],
-                    
-                
+            id: 0,
+            surveyName: "Quest4Change",
+            surveyAuthor: "Aaron J. Goldstein",
+            numQuestions: "4",
+            creationDate: "10/27/2022",
+            Questions: ["ID", "Age", "Likert1", "Likert2"],
+            Entry: ["AJG", "22", "3", "5"],
         },
         {
-                    id:1,
-                    surveyName: "Quest4Change2",
-                    surveyAuthor: "Carter Johnston ",
-                    numQuestions: "4",
-                    creationDate: "10/27/2022",
-                    Questions: ['ID', 'Age', 'Likert1', 'Likert2' ],
-                    Entry: ['CJ', '22', '3', '5'],
-        
-            
+            id: 1,
+            surveyName: "Quest4Change2",
+            surveyAuthor: "Carter Johnston ",
+            numQuestions: "4",
+            creationDate: "10/27/2022",
+            Questions: ["ID", "Age", "Likert1", "Likert2"],
+            Entry: ["CJ", "22", "3", "5"],
         },
         {
-                    id:2,
-                    surveyName: "Quest4Change3",
-                    surveyAuthor: "Jacob Schipman",
-                    numQuestions: "4",
-                    creationDate: "10/27/2022",
-                    Questions: ['ID', 'Age', 'Likert1', 'Likert2' ],
-                    Entry: ['JAS', '22', '3', '5'],
-                    
-            
+            id: 2,
+            surveyName: "Quest4Change3",
+            surveyAuthor: "Jacob Schipman",
+            numQuestions: "4",
+            creationDate: "10/27/2022",
+            Questions: ["ID", "Age", "Likert1", "Likert2"],
+            Entry: ["JAS", "22", "3", "5"],
         },
         {
-                    id:3,
-                    surveyName: "Quest4Change4",
-                    surveyAuthor: "Dhori Mato",
-                    numQuestions: "4",
-                    creationDate: "10/27/2022",
-                    Questions: ['ID','Age','Likert1','Likert2'],
-                    Entry: ['DM', '22', '3', '5'],
-                    
-                    
-            
+            id: 3,
+            surveyName: "Quest4Change4",
+            surveyAuthor: "Dhori Mato",
+            numQuestions: "4",
+            creationDate: "10/27/2022",
+            Questions: ["ID", "Age", "Likert1", "Likert2"],
+            Entry: ["DM", "22", "3", "5"],
         },
-        
     ];
-    
+
     // function questionSplit(){
     //    for (index1 in surveyList.Survey){
     //     for(index2 in surveyList.Survey[index1].Questions){
-            
+
     //         return surveyList.Survey[index1].Questions[index2];
     //     }
     //    }
-       
+
     // }
     // function entrySplit(){
     //     for (index1 in surveyList.Survey){
@@ -80,51 +63,42 @@
     //             }
     //         }
     //    }
-       
+
     // }
-        function Qslot(index){
+    function Qslot(index) {
+        let x = surveyList[index].Questions.length;
+        let y = surveyList[index].Entry.length;
+        console.log(y);
+        var Header = [];
+        var Responses = [];
+        for (var i = 0; i < x; i++) {
+            var j = surveyList[index].Questions[i];
+            Header[i] = j;
+        }
 
-            let x = surveyList[index].Questions.length;
-            let y = surveyList[index].Entry.length;
-            console.log(y);
-            var Header = [];
-            var Responses = [];
-            for (var i = 0; i < x; i++){
-                var j = surveyList[index].Questions[i];
-                Header[i] = j;
-                
-            }
+        for (var k = 0; k < y; k++) {
+            var l = surveyList[index].Entry[k];
+            Responses[k] = l;
+            //var Responses = surveyList.map(row => ({Answer: l}));
+        }
+        const row = Object.entries(Header);
+        console.log(typeof row);
+        var top = surveyList.map((row) => ({ Question: Header.join(",") }));
+        console.log(top);
+        var worksheet = XLSX.utils.json_to_sheet(top);
+        var workbook = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(workbook, worksheet, "Survey Results");
+        return workbook;
+    }
+    function getValue(Header) {
+        return Header.values;
+    }
 
-            
-            
-            for(var k = 0; k < y; k++){
-                var l = surveyList[index].Entry[k];
-                Responses[k] = l;
-                //var Responses = surveyList.map(row => ({Answer: l}));
-                
-            }
-            const row = Object.entries(Header);
-            console.log(typeof row);
-            var top = surveyList.map(row => ({Question: Header.join(",")}));
-            console.log(top);
-            var worksheet = XLSX.utils.json_to_sheet(top);
-            var workbook = XLSX.utils.book_new();
-            XLSX.utils.book_append_sheet(workbook, worksheet, "Survey Results");
-            return workbook;
-         }
-         function getValue(Header){
-            return Header.values
-         }
-       
-
-
-
-
-
-    
-    function ExcelExport(index){
-        let x = index
-        XLSX.writeFile(Qslot(index), ''+surveyList[x].surveyName+'.xlsx',{compression: true});
+    function ExcelExport(index) {
+        let x = index;
+        XLSX.writeFile(Qslot(index), "" + surveyList[x].surveyName + ".xlsx", {
+            compression: true,
+        });
     }
     // function returnSurvey(index){
     //     for(var i = 0; i < surveyList.length; i++){
@@ -160,54 +134,50 @@
     //             var view = new Uint8Array(buf);
     //             for(var i=0; i<s.length; i++) view[i] = s.charCodeAt(i) & 0xFF;
     //             return buf;
-    //         }  
-            
+    //         }
+
     //          function ExcelExport(){
     //             saveAs(new Blob([s2ab(wbout)],{type:"application/octet-stream"}),''+ SN + '.xlsx');
     //         }
 </script>
 
-
-<h1 class="header">Surveys ready to export</h1>
-
-
-<div class="list-group">
-    
-    {#each surveyList as survey, index}
-        <div class="list-group-item list-group-item-action">
-            <span>Name: {survey.surveyName}</span>
-            <span> ID: {survey.id}</span>
-            <span>Author: {survey.surveyAuthor}</span>
-            <span>Num Questions: {survey.numQuestions}</span>
-            <span>Creation Date: {survey.creationDate}</span>
-            <button on:click = {() => ExcelExport(index)}>Download as Excel</button>
-            <button class="btn-secondary" id = "ExportPDF">Download as PDF</button>
-            <button class="btn-danger">Delete</button>
-            
-        </div>
-    {:else}
-        <p>There are no existing surveys</p>
-    {/each}
+<div class="ms-5 me-5 mt-2">
+    <h1 class="mb-3">Analysis</h1>
+    <p class="lead ms-3">
+        Here is a list of surveys currently ready for export
+    </p>
+    <div class="list-group">
+        {#each surveyList as survey, index}
+            <div class="container border rounded p-2 m-2">
+                <div class="d-flex p-2 ms-1">Name: {survey.surveyName}</div>
+                <div class="d-flex p-2 ms-1">ID: {survey.id}</div>
+                <div class="d-flex p-2 ms-1">
+                    <div class="flex-fill">Author: {survey.surveyAuthor}</div>
+                    <div class="flex-fill">
+                        Creation Date: {survey.creationDate}
+                    </div>
+                </div>
+                <div class="d-flex p-2 ms-1">
+                    Num Questions: {survey.numQuestions}
+                </div>
+                <div class="d-flex p-2">
+                    <button
+                        class="btn btn-outline-success me-2"
+                        on:click={() => ExcelExport(index)}
+                        >Download as Excel</button
+                    >
+                    <button
+                        class="btn btn-outline-secondary me-2"
+                        id="ExportPDF">Download as PDF</button
+                    >
+                    <button class="btn btn-outline-danger me-2">Delete</button>
+                </div>
+            </div>
+        {:else}
+            <p>There are no existing surveys</p>
+        {/each}
+    </div>
 </div>
-<!-- <div>
-    <button on:click = {returnSurvey(index)}>Click</button>
-</div> -->
-
-<style>
-    span {
-        display: flex;
-    }
-</style>
-
-
-
-
-
-
-
-
-
-
 <!-- function Qslot(index){
 
     let x = surveyList[index].Questions.length;
@@ -233,3 +203,7 @@
     XLSX.utils.book_append_sheet(workbook, worksheet, "Survey Results");
     return workbook;
  } -->
+
+<!-- <div>
+    <button on:click = {returnSurvey(index)}>Click</button>
+</div> -->
